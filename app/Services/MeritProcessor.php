d2994a4e6d940f166996f9ee465366532a62309d
Fazlul Hoque <sawravm299@gmail.com>
2025-11-17 22:13:55 +0600

    merit process response update

--- app/Services/MeritProcessor.php
+++ app/Services/MeritProcessor.php
@@ -60,23 +60,18 @@ class MeritProcessor
         $all = collect($finalMerit);
 
         return [
-            'status' => 'success',
-            'message' => 'Merit Calculated Successfully',
-            'results' => [
-                'status' => 'success',
-                'data' => [
-                    'all_students'      => $finalMerit,
-                    'total_students'    => $results->count(),
-                    'merit_type'        => $meritType,
-                    'grouped_by'        => $groupBy,
+            'data' => [
+                'all_students'      => $finalMerit,
+                'total_students'    => $results->count(),
+                'merit_type'        => $meritType,
+                'grouped_by'        => $groupBy,
 
-                    // Separate Lists
-                    'section_wise'   => $all->groupBy('section')->map->values()->toArray(),
-                    'shift_wise'     => $all->groupBy('shift')->map->values()->toArray(),
-                    'group_wise'     => $all->groupBy('group')->map->values()->toArray(),
-                    'gender_wise'    => $all->groupBy('gender')->map->values()->toArray(),
-                    'religion_wise'  => $all->groupBy('religion')->map->values()->toArray(),
-                ]
+                // Separate Lists
+                'section_wise'   => $all->groupBy('section')->map->values()->toArray(),
+                'shift_wise'     => $all->groupBy('shift')->map->values()->toArray(),
+                'group_wise'     => $all->groupBy('group')->map->values()->toArray(),
+                'gender_wise'    => $all->groupBy('gender')->map->values()->toArray(),
+                'religion_wise'  => $all->groupBy('religion')->map->values()->toArray(),
             ]
         ];
     }
