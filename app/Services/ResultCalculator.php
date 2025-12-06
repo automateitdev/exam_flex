9659706bdc05020dff403b4a77b0fbf42bedde8a
Fazlul Hoque <sawravm299@gmail.com>
2025-12-06 17:40:16 +0600

    result process update x10

--- app/Services/ResultCalculator.php
+++ app/Services/ResultCalculator.php
@@ -202,10 +202,11 @@ class ResultCalculator
             } else {
                 $single = $this->processSingle($first, $gradeRules, $mark_configs);
                 // $single['is_optional'] = ($first['subject_id'] == $optionalId);
-                // if($first['subject_id'] == $optionalId) {
-                   Log::info($first['subject_id']);
-                   Log::info($optionalId);
-                // }
+                Log::channel('exam_flex_log')->info('Debug', [
+                    'subject_id' => $first['subject_id'],
+                    'optional_id' => $optionalId,
+                    'is_optional' => ($first['subject_id'] == $optionalId)
+                ]);
                 $merged[] = $single;
 
                 if (!$single['is_uncountable']) {
